ba44ea510acda194cf301ab595b3ef48303bb241
FengWenxi <ThorfromAsgard@outlook.com>
2024-10-17 15:50:29 +0800

    modify blocking queue

--- src/base/blocking_queue.hpp
+++ src/base/blocking_queue.hpp
@@ -24,7 +24,7 @@ namespace cybertron::base {
 template <typename T>
 /**
  * @brief An implementation of blocking queue. The queue works in two modes by specifying the {push_block} parameter.
- * #NOTE Use it carefully when set the parameter {max_capacity} to 0 because it may lead to unlimited memory
+ * #NOTE Use it carefully when set the parameter {capacity_limit} to 0 because it may lead to unlimited memory
  * consumption.
  *
  */
@@ -32,21 +32,25 @@ template <typename T>
 class BlockingQueue : public Noncopyable {
 public:
     /**
-     * @brief Construct a new Blocking Queue object, use it carefully when set the parameter {max_capacity} to 0.
+     * @brief Construct a new Blocking Queue object, use it carefully when set the parameter {capacity_limit} to 0.
      *
-     * @param push_block Whether the push method will work in blocking mode or
-     * not.
+     * @param push_block Whether the push method will work in blocking mode or not.
      *
-     * @param max_capacity Again, you should be very aware of what you are doing when set it to 0, if you are not sure,
-     * then you are not aware !!!!!!
+     * @param capacity_limit The capacity limit of the queue, again, you should be very aware of what you are doing when
+     * set it to 0, if you are not sure, then you are not aware !!!!!!
      *
      */
-    explicit BlockingQueue(bool push_block = false, size_t max_capacity = 0)
-        : push_block_(push_block), capacity_limit_(max_capacity), mutex_(), consumer_(mutex_), deque_(), active_(true) {
+    explicit BlockingQueue(bool push_block = false, size_t capacity_limit = 0)
+        : push_block_(push_block),
+          capacity_limit_(capacity_limit),
+          mutex_(),
+          consumer_(mutex_),
+          deque_(),
+          active_(true) {
         std::cout << "Blocking Queue capacity: " << capacity_limit_ << std::endl;
         if (capacity_limit_ == 0) {
             // TODO: Find another way to warning!
-            std::cout << "Warning! Blocking queue parameter {max_capacity} is set to 0, may cause out of memory."
+            std::cout << "Warning! Blocking queue parameter {capacity_limit} is set to 0, may cause out of memory."
                       << std::endl;
         }
     }
@@ -67,13 +71,13 @@ public:
     }
 
     /**
-     * @brief Push element to the back of the queue within {timeout} microseconds in a blocking way.If the {timeout}
-     * parameter is set to 0,then it will always try to push until success or the queue is closed.
+     * @brief Push element to the back of the queue within {timeout} microseconds in a blocking way. If the {timeout}
+     * parameter is set to 0, then it will always try to push until success or the queue is closed.
      *
      * @param element Input element that is going to be pushed.
      * @param timeout in microseconds.
-     * @return true if the function is not always full in {timeout} microseconds and successfully pushes one element to
-     * the back of the queue,
+     * @return true if the function is not always full during {timeout} microseconds and successfully pushes one element
+     * to the back of the queue,
      * @return false if it fails.
      */
     bool PushBack(const T& element, const int64_t& timeout = 0) {
@@ -103,13 +107,13 @@ public:
     }
 
     /**
-     * @brief Push element to the back of the queue within {timeout} microseconds in a blocking way.If the {timeout}
-     * parameter is set to 0,then it will always try to push until success or the queue is closed.
+     * @brief Push element to the back of the queue within {timeout} microseconds in a blocking way. If the {timeout}
+     * parameter is set to 0, then it will always try to push until success or the queue is closed.
      *
-     * @param element Input elemet that is going to be pushed.
+     * @param element Input element that is going to be pushed.
      * @param timeout in microseconds.
-     * @return true if the function is not always empty in {timeout} microseconds and successfully pushes one element to
-     * the back of the queue,
+     * @return true if the function is not always full during {timeout} microseconds and successfully pushes one element
+     * to the back of the queue,
      * @return false if it fails.
      */
     bool PushBack(T&& element, const uint64_t& timeout = 0) {
@@ -139,14 +143,16 @@ public:
     }
 
     /**
-     * @brief Push item to the front of the queue within {timeout} microseconds in a blocking way.
+     * @brief Push element to the front of the queue within {timeout} microseconds in a blocking way. If the {timeout}
+     * parameter is set to 0, then it will always try to push until success or the queue is closed.
      *
-     * @param item Input elemet that is going to be pushed.
+     * @param element Input element that is going to be pushed.
      * @param timeout Timeout in microseconds.
-     * @return true if the function successfully pushes one element to the front of the queue,
+     * @return true if the function is not always full during {timeout} microseconds and successfully pushes one element
+     * to the front of the queue,
      * @return false if it fails.
      */
-    bool PushFront(const T& item, const uint64_t& timeout = 0) {
+    bool PushFront(const T& element, const uint64_t& timeout = 0) {
         std::unique_lock<std::mutex> lock(mutex_);
         if (push_block_) {
             std::cv_status is_timeout = std::cv_status::no_timeout;
@@ -167,16 +173,18 @@ public:
                 deque_.pop_back();
             }
         }
-        deque_.push_front(item);
+        deque_.push_front(element);
         consumer_.notify_one();
     }
 
     /**
-     * @brief Push item to the front of the queue within {timeout} microseconds in a blocking way.
+     * @brief Push element to the front of the queue within {timeout} microseconds in a blocking way. If the {timeout}
+     * parameter is set to 0, then it will always try to push until success or the queue is closed.
      *
-     * @param item Input elemet that is going to be pushed.
+     * @param element Input element that is going to be pushed.
      * @param timeout Timeout in microseconds.
-     * @return true if the function successfully pushes one element to the front of the queue,
+     * @return true if the function is not always full during {timeout} microseconds and successfully pushes one element
+     * to the front of the queue,
      * @return false if it fails.
      */
     bool PushFront(T&& item, const uint64_t& timeout = 0) {
@@ -196,7 +204,7 @@ public:
                 return false;
             }
         } else {
-            if (capacity_limit_ && deque_.size() >= capacity_limit_) {
+            while (capacity_limit_ && deque_.size() >= capacity_limit_) {
                 deque_.pop_back();
             }
         }
@@ -205,27 +213,28 @@ public:
     }
 
     /**
-     * @brief Pop front element within {timeout} microseconds in a blocking way.
+     * @brief Pop the front element of the queue within {timeout} microseconds in a blocking way. If the {timeout}
+     * parameter is set to 0, then it will always try to pop until success or the queue is closed.
      *
-     * @param item output item.
-     * @param timeout in microseconds.
-     * @return true if the function is not always empty in {timeout} microseconds and successfully pops one element from
-     * the front of the queue,
+     * @param element Output element.
+     * @param timeout Timeout in microseconds.
+     * @return true if the function is not always empty during {timeout} microseconds and successfully pops one element
+     * from the front of the queue,
      * @return false if it fails.
      */
-    bool PopFront(T& item, const int& timeout = 0) {
+    bool PopFront(T& element, const int& timeout = 0) {
         std::unique_lock<std::mutex> lock(mutex_);
         std::cv_status is_timeout = std::cv_status::no_timeout;
         if (timeout) {
             is_timeout = consumer_.wait_for(lock, std::chrono::microseconds(timeout),
-                                            [&] { return ((!active_) || (deque_.empty())); });
+                                            [&] { return ((!active_) || (!deque_.empty())); });
         } else {
             consumer_.wait(lock, [&] { return ((!active_) || (!deque_.empty())); });
         }
         if ((!active_) || (is_timeout == std::cv_status::timeout)) {
             return false;
         };
-        item = deque_.front();
+        element = deque_.front();
         deque_.pop_front();
         if (push_block_) {
             producer_.notify_one();
@@ -234,26 +243,28 @@ public:
     }
 
     /**
-     * @brief Pop back element within {timeout} microseconds in a blocking way.
+     * @brief Pop the back element of the queue within {timeout} microseconds in a blocking way. If the {timeout}
+     * parameter is set to 0, then it will always try to pop until success or the queue is closed.
      *
-     * @param item output item.
-     * @param timeout in microseconds.
-     * @return true if the function is not empty in {timeout} microseconds and successfully pops one element from the
-     * back of the queue,
+     * @param element Output element.
+     * @param timeout Timeout in microseconds.
+     * @return true if the function is not always empty during {timeout} microseconds and successfully pops one element
+     * from the back of the queue,
      * @return false if it fails.
      */
-    bool PopBack(T& item, const int& timeout = 0) {
+    bool PopBack(T& element, const int& timeout = 0) {
+        std::unique_lock<std::mutex> lock(mutex_);
         std::cv_status is_timeout = std::cv_status::no_timeout;
         if (timeout) {
             is_timeout = consumer_.wait_for(lock, std::chrono::microseconds(timeout),
-                                            [&] { return ((!active_) || (deque_.empty())); });
+                                            [&] { return ((!active_) || (!deque_.empty())); });
         } else {
             consumer_.wait(lock, [&] { return ((!active_) || (!deque_.empty())); });
         }
         if ((!active_) || (is_timeout == std::cv_status::timeout)) {
             return false;
         };
-        item = deque_.back();
+        element = deque_.back();
         deque_.pop_back();
         if (push_block_) {
             producer_.notify_one();
